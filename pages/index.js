dfcc8d7d463b89c8bb609aec89ac1c925bcb515a
Iconicbaba1 <anilkumarekka51@gmail.com>
2025-06-14 20:48:31 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '40px' }}>
+      <h1>Welcome to LiQUiD ANiME</h1>
+      <p>Your Next.js site is working!</p>
+    </div>
+  );
+}
